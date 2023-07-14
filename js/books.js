463d41d37c890a2398d47b103e77b4f1f7c2cf7e
Supriya Kuncha <kunchasupriya181@gmail.com>
2023-07-14 11:23:54 +0530

    changes

--- js/books.js
+++ js/books.js
@@ -1,68 +1,163 @@
-
-
-
-let bookContainer = document.querySelector(".cards-flex");
+let bookContainer = document.querySelector(".list-book");
 let searchBooks = document.getElementById("search-box");
 
 const getBooks = async (book) => {
-    const response = await fetch(
-        `https://www.googleapis.com/books/v1/volumes?q=${book}`
-    );
-    const data = await response.json();
-    console.log(data);
-    return data;
+  const response = await fetch(
+    `https://www.googleapis.com/books/v1/volumes?q=${book}`
+  );
+  const data = await response.json();
+  console.log(data);
+  return data;
 };
 
 const extractThumbnail = ({ imageLinks }) => {
-    const DEFAULT_THUMBNAIL = "/assets/logo-light.svg";
-    if (!imageLinks || !imageLinks.thumbnail) {
-        return DEFAULT_THUMBNAIL;
-    }
-    return imageLinks.thumbnail.replace("http://", "https://");
+  const DEFAULT_THUMBNAIL = "/assets/logo-light.svg";
+  if (!imageLinks || !imageLinks.thumbnail) {
+    return DEFAULT_THUMBNAIL;
+  }
+  return imageLinks.thumbnail.replace("http://", "https://");
 };
 
+// This function populates the search
 const drawListBook = async () => {
-    console.log(searchBooks.value);
-    if (searchBooks.value != "") {
-        bookContainer.style.display = "flex";
-        bookContainer.innerHTML = `<div class='prompt'><div class="loader"></div></div>`;
-        const data = await getBooks(`${searchBooks.value}&maxResults=20`);
-        if (data.error) {
-            bookContainer.innerHTML = `<div class='prompt'>ツ Limit exceeded! Try after some time</div>`;
-        } else if (data.totalItems == 0) {
-            bookContainer.innerHTML = `<div class='prompt'>ツ No results, try a different term!</div>`;
-        } else if (data.totalItems == undefined) {
-            bookContainer.innerHTML = `<div class='prompt'>ツ Network problem!</div>`;
-        } else {
-            bookContainer.innerHTML = data.items
-                .map(
-                    ({ volumeInfo }) =>
-                        `<div class="card-items books">
+  console.log(searchBooks.value);
+  if (searchBooks.value != "") {
+    bookContainer.style.display = "flex";
+    bookContainer.style.justifyContent = "center";
+    bookContainer.innerHTML = `<div class='prompt'><div class="loader"></div></div>`;
+    const data = await getBooks(`${searchBooks.value}&maxResults=20`);
+    if (data.error) {
+      bookContainer.innerHTML = `<div class='prompt'>ツ Limit exceeded! Try after some time</div>`;
+    } else if (data.totalItems == 0) {
+      bookContainer.innerHTML = `<div class='prompt'>ツ No results, try a different term!</div>`;
+    } else if (data.totalItems == undefined) {
+      bookContainer.innerHTML = `<div class='prompt'>ツ Network problem!</div>`;
+    } else {
+      bookContainer.innerHTML = data.items
+        .map(
+          ({ volumeInfo }) =>
+            `<div class="search-card-items books">
                     <img
                         src="` +
-                        extractThumbnail(volumeInfo) +
-                        `"
+            extractThumbnail(volumeInfo) +
+            `"
+                        alt="book.png"
+                        class="search-book-img"
+                    />
+                    <div class="search-card-description">
+                        <h1 class="search-bookName">${
+                          volumeInfo.title.slice(0, 20) + "..."
+                        }</h1>
+                        <h2 class="search-author">${volumeInfo.authors}</h2>
+                        <div class="search-container">
+                            <div class="genre-name">
+                                <span class="search-description-title"
+                                    >Genre:</span
+                                >
+                                <span class="search-update" id="title-update"
+                                    >Romantic</span
+                                >
+                            </div>
+                            <div class="yop">
+                                <span class="search-description-title"
+                                    >Yop:</span
+                                >
+                                <span class="search-update" id="yop-update"
+                                    >${volumeInfo.publishedDate}</span
+                                >
+                            </div>
+                            <div class="available">
+                                <span class="search-description-title"
+                                    >Available Copies:</span
+                                >
+                                <span class="search-update" id="copies-update"
+                                    >25</span
+                                >
+                            </div>
+                            <div class="origin">
+                                <span class="search-description-title"
+                                    >Language:</span
+                                >
+                                <span class="search-update" id="origin-update"
+                                    >${volumeInfo.language}</span
+                                >
+                            </div>
+                        </div>
+                        <button type="submit" class="search-addToBag">
+                            Add to Bag
+                        </button>
+                    </div>
+                </div>
+            </div>`
+        )
+        .join("");
+    }
+  } else {
+    bookContainer.style.display = "none";
+  }
+};
+
+const updateFilter = ({ innerHTML }, f) => {
+  document.getElementById("main").scrollIntoView({
+    behavior: "smooth",
+  });
+  let m;
+  switch (f) {
+    case "author":
+      m = "inauthor:";
+      break;
+    case "subject":
+      m = "subject:";
+      break;
+  }
+  searchBooks.value = m + innerHTML;
+  debounce(drawListBook, 1000);
+};
+
+const debounce = (fn, time, to = 0) => {
+  to ? clearTimeout(to) : (to = setTimeout(drawListBook, time));
+};
+searchBooks.addEventListener("input", () => debounce(drawListBook, 1000));
+
+// This function populates the page with the cards
+let pageGrid = document.querySelector(".cards-flex");
+
+const pageListBook = async () => {
+  console.log(searchBooks.value);
+  pageGrid.innerHTML = `<div class='prompt'><di v class="loader"></div></div>`;
+  const data = await getBooks(`motivation&maxResults=38`);
+  if (data.error) {
+    pageGrid.innerHTML = `<div class='prompt'>ツ Limit exceeded! Try after some time</div>`;
+  } else if (data.totalItems == 0) {
+    pageGrid.innerHTML = `<div class='prompt'>ツ No results, try a different term!</div>`;
+  } else if (data.totalItems == undefined) {
+    pageGrid.innerHTML = `<div class='prompt'>ツ Network problem!</div>`;
+  } else {
+    pageGrid.innerHTML = data.items
+      .map(
+        ({ volumeInfo }) =>
+          `<div class="card-items books">
+                    <img
+                        src="` +
+          extractThumbnail(volumeInfo) +
+          `"
                         alt="book.png"
                         class="book-img"
                     />
-                    <div class="search-description">
-                        <h1 class="bookName">${
-                            volumeInfo.title.slice(0, 20) + "..."
-                        }</h1>
+                    <div class="description">
+                        <h1 class="bookName">
+                            ${volumeInfo.title.slice(0, 40) + "..."}
+                        </h1>
                         <h2 class="author">${volumeInfo.authors}</h2>
                         <div class="container">
                             <div class="genre-name">
-                                <span class="description-title"
-                                    >Genre:</span
-                                >
+                                <span class="description-title">Genre:</span>
                                 <span class="update" id="title-update"
                                     >Romantic</span
                                 >
                             </div>
                             <div class="yop">
-                                <span class="description-title"
-                                    >Yop:</span
-                                >
+                                <span class="description-title">Yop:</span>
                                 <span class="update" id="yop-update"
                                     >${volumeInfo.publishedDate}</span
                                 >
@@ -72,7 +167,7 @@ const drawListBook = async () => {
                                     >Available Copies:</span
                                 >
                                 <span class="update" id="copies-update"
-                                    >25</span
+                                    >${Math.floor(Math.random() * 21)}</span
                                 >
                             </div>
                             <div class="origin">
@@ -88,54 +183,46 @@ const drawListBook = async () => {
                             Add to Bag
                         </button>
                     </div>
-                </div>
-            </div>`
-            )
-            .join("");
-        }
-    } else {
-        bookContainer.style.display = "none";
-    }
+                </div>`
+      )
+      .join("");
+  }
 };
 
-const updateFilter = ({ innerHTML }, f) => {
-    document.getElementById("main").scrollIntoView({
-        behavior: "smooth",
-    });
-    let m;
-    switch (f) {
-        case "author":
-            m = "inauthor:";
-            break;
-        case "subject":
-            m = "subject:";
-            break;
-    }
-    searchBooks.value = m + innerHTML;
-    debounce(drawListBook, 1000);
+const pageupdateFilter = ({ innerHTML }, f) => {
+  document.getElementById("main").scrollIntoView({
+    behavior: "smooth",
+  });
+  let m;
+  switch (f) {
+    case "author":
+      m = "inauthor:";
+      break;
+    case "subject":
+      m = "subject:";
+      break;
+  }
+  searchBooks.value = m + innerHTML;
+  pageDebounce(pageListBook, 1000);
 };
 
-const debounce = (fn, time, to = 0) => {
-    to ? clearTimeout(to) : (to = setTimeout(drawListBook, time));
+const pageDebounce = (fn, time, to = 0) => {
+  to ? clearTimeout(to) : (to = setTimeout(pageListBook, time));
 };
-searchBooks.addEventListener("input", () => debounce(drawListBook, 1000));
+pageDebounce(pageListBook, 1000);
 
-// const API_KEY = "AIzaSyA0n3bCT4kqrLJnD8hF2-iacgX70Y3VWNc";
+const searchDiv = document.querySelector("input");
+const listBook = document.querySelector(".list-book");
 
-// function searchBooks(query) {
-//     const url = `GET https://www.googleapis.com/books/v1/volumes?q=${query}`;
-//     const request = new XMLHttpRequest();
-//     request.open("GET", url);
-//     request.setRequestHeader("Authorization", `Bearer ${API_KEY}`);
-//     request.onload = function () {
-//         if (request.status === 200) {
-//             const data = JSON.parse(request.responseText);
-//             console.log(data);
-//         } else {
-//             console.log("Error: " + request.statusText);
-//         }
-//     };
-//     request.send();
-// }
+searchDiv.addEventListener("click", () => {
+  if (searchBooks.value != "") {
+    listBook.style.display = "block";
+  }
+});
 
-// searchBooks("s");
+document.addEventListener("click", function (event) {
+  if (!searchDiv.contains(event.target)) {
+    //hide things classes.. yourContainer.classList.add('hidden');
+    listBook.style.display = "none";
+  }
+});
